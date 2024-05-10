77dc7aeab622918f1172b2ae20b37c65c25404f9
Guiomar Almunia Herranz <galmuniaia@gmail.com>
2024-05-10 15:35:17 +0200

    (01:color-flipper ) changes

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,28 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+
+const colors = ["skyblue", "coral", "green", "red", "lavender", "mintcream"];
+
+const btn = document.getElementById("btn")
+const text = document.querySelector("text")
+const color = document.querySelector(".color")
+const btn2 =document.getElementById("btn-2")
+
+btn.addEventListener("click", () => {
+    const randonNumber = getRandomNumber();
+     console.log(randonNumber)
+     document.body.style.backgroundColor = colors[randonNumber];
+     color.textContent = colors[randonNumber];
+    //  console.log(text);
+    
+})
+
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
+
+
+// btn2.addEventListener("click", () => {
+//     console.log('Hello from btn2')
+// })
+
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,31 @@
+
+
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color")
+console.log(color);
+
+btn.addEventListener("click", () => {
+       getRandomColor()
+    
+})
+
+// porque el bucle corre 6 veces? : porque en hexadecimal los valores se representan  como #f1f5f2
+function getRandomColor() {
+    let hexColor = "#"
+    for(let i=0; i<6; i++) {
+       // hexColor += hex[15];
+        hexColor += hex[getRandomNumber()];
+        console.log('hexColor::', hexColor);  
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+}
+
+
+   function getRandomNumber() {
+       return Math.floor(Math.random() * hex.length);
+   }
+
